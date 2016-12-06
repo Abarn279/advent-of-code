60c2d60a6b09c857cbb9461947c9a775d2ab69a0
Abarnett <Abarnett@echosolutionsgroup.com>
2016-12-06 09:18:18 -0600

    6a done

---
+++ src/_6a.py
@@ -0,0 +1,16 @@
+from file_importer import FileImporter
+import collections
+
+# Get input
+inp = [i.strip() for i in FileImporter.get_input("/../input/6a.txt").split("\n")]
+
+# Transpose input to columns
+columns = []
+for column in range(0, len(inp[0])):
+    columnToAdd = []
+    for row in range(0, len(inp)):
+        columnToAdd.append(inp[row][column])
+    columns.append("".join(columnToAdd))
+
+corrected = "".join([collections.Counter(column).most_common()[0][0] for column in columns])
+print(corrected)
