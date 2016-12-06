987827762f497ba488bcfa4eb280b00f80c22804
Abarnett <Abarnett@echosolutionsgroup.com>
2016-12-06 10:57:34 -0600

    used built in python zip

--- src/_6a.py
+++ src/_6a.py
@@ -4,13 +4,6 @@ import collections
 # Get input
 inp = [i.strip() for i in FileImporter.get_input("/../input/6a.txt").split("\n")]
 
-# Transpose input to columns
-columns = []
-for column in range(0, len(inp[0])):
-    columnToAdd = []
-    for row in range(0, len(inp)):
-        columnToAdd.append(inp[row][column])
-    columns.append("".join(columnToAdd))
-
-corrected = "".join([collections.Counter(column).most_common()[0][0] for column in columns])
+# Transpose input to columns, get most common for each column
+corrected = "".join([collections.Counter(column).most_common()[0][0] for column in zip(*inp)])
 print(corrected)
