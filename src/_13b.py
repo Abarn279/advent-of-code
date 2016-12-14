16d4c981223b49c8545a6e58d2cd385701062337
Abarnett <Abarnett@echosolutionsgroup.com>
2016-12-14 16:51:44 -0600

    13 done

--- src/_13b.py
+++ src/_13b.py
@@ -1,6 +1,5 @@
 from file_importer import FileImporter
 from queue import Queue
-import sys
 
 # Get input
 fav_number = int(FileImporter.get_input("/../input/13a.txt"))
@@ -13,23 +12,29 @@ def check_position(x, y):
     return True if binary_rep.count("1") % 2 == 0 else False
 
 start = (1, 1, 0)
-end = (31, 39)
 visited = []
 q = Queue()
 q.put(start)
+count = 0
 
 while not q.empty():
     position = q.get()
+
+    if (position[0], position[1]) in visited:
+        continue 
+
     visited.append( (position[0], position[1]) )
+
+    if position[2] > 50:
+        break
+    count += 1
 
     for i in range(-1, 2):
         for j in range(-1, 2):
             x, y, dist = position[0] + i, position[1] + j, position[2] + 1
             
             if abs(i) != abs(j):
-                if (x, y) == end:
-                    print(dist)
-                    sys.exit()
-
-                if (x, y) not in visited and check_position(x, y):
+                if check_position(x, y):
                     q.put( (x, y, dist) )
+
+print(count)
