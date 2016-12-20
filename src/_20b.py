0554664757850a82617abba1307c28d504f14b81
Abarnett <Abarnett@echosolutionsgroup.com>
2016-12-20 11:33:11 -0600

    20b done

---
+++ src/_20b.py
@@ -0,0 +1,19 @@
+from file_importer import FileImporter
+
+# Get input
+inp =  [list(map(int, y.split("-"))) for y in [x.strip() for x in FileImporter.get_input("/../input/20a.txt").split("\n")]]
+inp = sorted(inp, key=lambda x: x[0])
+
+max0, max1 = inp[0][0], inp[0][1]
+allowed = 0
+for i in inp:
+    if i[0] > max0:
+        max0 = i[0]
+
+    if max0 > max1 + 1:
+        allowed += 1
+    
+    if i[1] > max1:
+        max1 = i[1]
+
+print(allowed)
